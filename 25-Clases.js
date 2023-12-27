1f2829ee8b112457743f6a00f31c59572287a869
Jesus Ruben Gamez Molina <jesus.gamez@coppel.ccom>
2023-12-26 21:44:05 -0700

    Se sube avance del curso de Universidad Js

--- 25-Clases.js
+++ 25-Clases.js
@@ -2,6 +2,7 @@
 //let persona2 = new Persona('','');
 
 /*Clase padre*/
+
 class Persona {
   constructor(nombre, apellido) {
     this._nombre = nombre;
@@ -14,11 +15,29 @@ class Persona {
     this._nombre = nombre;
   }
 
-  get appellido() {
+  get apellido() {
     return this._apellido;
   }
-  set apelido(apellido) {
+  set apellido(apellido) {
     this._apellido = apellido;
+  }
+  nombreCompleto(){
+    return this._nombre +' '+ this._apellido;
+  }
+  //Sobreescribiendo el metodo de la clase Padre Object
+  toString()
+  {
+    //Se aplica polimorfismo(multiples formas de ejecución)
+    //el metodo se ejecuta depende si es una referencia de tipo padre o hijo 
+    return this.nombreCompleto();
+  }
+  static saludar()
+  {
+    console.log('Saludos desde el metodo static');
+  }
+  static saludar2(persona)
+  { 
+     console.log(persona.nombre +' '+ persona.apellido);
   }
 }
 
@@ -33,14 +52,27 @@ class Empleado extends Persona {
   set departamento(departamento){
     return this._departamento = departamento;
   }
+  //Sobreescritura
+  nombreCompleto(){
+    return super.nombreCompleto() +' '+this._departamento;
+  }
 }
 
 let persona1 = new Persona("Ruben", "Velez");
 persona1.nombre = "Rosa"; //Se manda a llamar el metodo set
-console.log(persona1.nombre); //Se manda a llamar el metodo get
+console.log(persona1); //Se manda a llamar el metodo get
 
 // let persona2 = new Persona("Juan", "p");
 // console.log(persona2);
 
 let empleado1 = new Empleado('Juan','Lopez','Sistemas')
-console.log(empleado1);
+console.log(empleado1.nombreCompleto());
+
+console.log(empleado1.toString())
+
+// No es posible llamar un metodo static desde un objeto     
+Persona.saludar();
+Persona.saludar2(persona1);
+
+Empleado .saludar();
+Empleado.saludar2(empleado1);
